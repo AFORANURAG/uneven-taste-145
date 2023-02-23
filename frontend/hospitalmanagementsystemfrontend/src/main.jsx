13bbfc91787c922820edc7e0bdfd24cb82375f66
AFORANURAG <anuragupadhyay172912313@gmail.com>
2023-02-23 16:20:06 +0530

    carousel added

--- frontend/hospitalmanagementsystemfrontend/src/main.jsx
+++ frontend/hospitalmanagementsystemfrontend/src/main.jsx
@@ -5,10 +5,6 @@ import { ChakraProvider } from '@chakra-ui/react'
 import {BrowserRouter} from "react-router-dom"
 import '@splidejs/react-splide/css/core';
 
-import '@fontsource/roboto/300.css';
-import '@fontsource/roboto/400.css';
-import '@fontsource/roboto/500.css';
-import '@fontsource/roboto/700.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
